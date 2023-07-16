baa66749733a95cfde6d82342af5d7d3d0814c14
presswintox <presswintox@gmail.com>
2023-07-17 00:40:02 +0200

    settings

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -2,11 +2,11 @@ from pydantic import BaseSettings, Field
 
 
 class TestSettings(BaseSettings):
-    es_host: str = Field('elastic', env='ELASTIC_HOST')
+    es_host: str = Field('localhost', env='ELASTIC_HOST')
     es_port: int = Field(9200, env='ELASTIC_PORT')
-    redis_host: str = Field('redis', env='REDIS_HOST')
+    redis_host: str = Field('localhost', env='REDIS_HOST')
     redis_port: int = Field(6379, env='REDIS_PORT')
-    service_url: str = 'http://backend:8000'
+    service_url: str = Field('http://localhost:8000', env='SERVICE_URL')
 
 
 test_settings = TestSettings()
